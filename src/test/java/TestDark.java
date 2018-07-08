0918b7ec157ee01905045b2db20af30530a3b557
Pyr0Joke <shmel9311@gmail.com>
2018-07-08 16:57:39 +0300

    + build test-results

--- src/test/java/TestDark.java
+++ src/test/java/TestDark.java
@@ -5,20 +5,32 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class TestDark {
+
+    FirefoxDriver driver;
+
+    @BeforeMethod
+    public void setUp(){
+        System.setProperty("webdriver.gecko.driver", "D:\\Gecko\\geckodriver.exe");
+        driver = new FirefoxDriver();
+    }
+
     @Test
     public void firstTest(){
-        System.setProperty("webdriver.gecko.driver", "D:\\Gecko\\geckodriver.exe");
-        WebDriver driver = new FirefoxDriver();
         driver.get("http://dark-world.ru/main/");
         driver.findElement(By.id("Login")).sendKeys("shmel9311@gmail.com");
         driver.findElement(By.id("Password")).sendKeys("458854qwe");
         driver.findElement(By.id("loginBtn")).click();
         WebElement dynamicElement = (new WebDriverWait(driver, 10)).until(ExpectedConditions.presenceOfElementLocated(By.className("usermenu_icon")));
         Assert.assertNotEquals(driver.findElement(By.linkText("Лучшие альбомы")),0);
-        driver.close();
+        //driver.close();
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
     }
 
 }
